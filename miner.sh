fca73479f5d6bafbed9a505d606fc20cf20945e4
Erik Rogers <erik.rogers@live.com>
2018-01-12 23:45:54 -0500

    Fix merge

--- miner.sh
+++ miner.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-<<<<<<< HEAD
-./creepMiner ${CREEP_MINER_DATADIR}/mining.conf
-
-=======
 ./creepMiner $CREEP_MINER_DATADIR/mining.conf
->>>>>>> 9bf51e4f8c76f873c1b11bacdbb4273fe32358b8
